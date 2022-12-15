c2f05e5cebb75e4958595d80feb025e19dde7eba
Porosh45 <rashedulhasanporosh45@gmail.com>
2022-12-15 16:21:24 +0600

    rollbacked to sqlite

--- blog/settings.py
+++ blog/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'postgres',
-    'USER' : 'postgres',
-    'PASSWORD' : 'rhp01748218277',
-    'HOST' : 'localhost',
-    'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': 'postgres',
+#     'USER' : 'postgres',
+#     'PASSWORD' : 'rhp01748218277',
+#     'HOST' : 'localhost',
+#     'PORT' : '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
